f0a037b80e2976245d75a124b416a69cc3426b29
Antonio Martinez <jose.antonio.martinez.torres@intel.com>
2024-10-11 19:46:37 -0700

    Adding mosquitto container and implementation of gvametapublish
    Signed-off-by: Antonio Martinez <jose.antonio.martinez.torres@intel.com>

--- src/pipelines/yolov8s_roi.sh
+++ src/pipelines/yolov8s_roi.sh
@@ -9,7 +9,9 @@ PRE_PROCESS="${PRE_PROCESS:=""}" #""|pre-process-backend=vaapi-surface-sharing|p
 AGGREGATE="${AGGREGATE:="gvametaaggregate name=aggregate !"}" # Aggregate function at the end of the pipeline ex. "" | gvametaaggregate name=aggregate
 PUBLISH="${PUBLISH:="name=destination file-format=2 file-path=/tmp/results/r$cid\"_gst\".jsonl"}" # address=localhost:1883 topic=inferenceEvent method=mqtt
 
-CLASS_IDS="0" # YOLOv8 classes to be detected example "0,1,30"
+CLASS_IDS="" # YOLOv8 classes to be detected example "0,1,30"
+MQTT_HOST="127.0.0.1:1883"
+ROI="BASKET"
 
 if [ "$RENDER_MODE" == "1" ]; then
     OUTPUT="${OUTPUT:="! videoconvert ! gvawatermark ! videoconvert ! fpsdisplaysink video-sink=ximagesink sync=true --verbose"}"
@@ -18,9 +20,15 @@ else
 fi
 
 echo "decode type $DECODE"
-echo "Run yolov8 pipeline with ROI on $DEVICE with batch size = $BATCH_SIZE"
+echo "Run YOLOv8 pipeline with ROI and Filtering on $DEVICE with batch size = $BATCH_SIZE"
 
-gstLaunchCmd="GST_DEBUG=\"GST_TRACER:7\" GST_TRACERS=\"latency_tracer(flags=pipeline,interval=100)\" gst-launch-1.0 $inputsrc ! $DECODE ! gvaattachroi mode=1 file-path=/home/pipeline-server/pipelines/roi.json ! gvadetect batch-size=$BATCH_SIZE model-instance-id=odmodel name=detection model=models/object_detection/yolov8s/FP32/yolov8s.xml device=$DEVICE $PRE_PROCESS inference-region=1 object-class=ROI1 threshold=0.5 ! gvapython module=/home/pipeline-server/extensions/object_filter.py class=ObjectDetectionFilter kwarg=\"{\\\"class_ids\\\": \\\"$CLASS_IDS\\\", \\\"rois\\\": \\\"ROI1\\\"}\" !  gvatrack ! $AGGREGATE gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/tmp/results/r$cid\"_gst\".jsonl $OUTPUT 2>&1 | tee >/tmp/results/gst-launch_$cid\"_gst\".log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /tmp/results/pipeline$cid\"_gst\".log)"
+gstLaunchCmd="GST_DEBUG=\"GST_TRACER:7\" GST_TRACERS=\"latency_tracer(flags=pipeline,interval=100)\" gst-launch-1.0 $inputsrc ! $DECODE ! gvaattachroi mode=1 file-path=/home/pipeline-server/pipelines/roi.json ! gvadetect batch-size=$BATCH_SIZE model-instance-id=odmodel name=detection model=models/object_detection/yolov8s/FP32/yolov8s.xml device=$DEVICE $PRE_PROCESS inference-region=1 object-class=$ROI threshold=0.5 ! \
+gvapython module=/home/pipeline-server/extensions/object_filter.py class=ObjectDetectionFilter kwarg=\"{\\\"class_ids\\\": \\\"$CLASS_IDS\\\", \\\"rois\\\": \\\"$ROI\\\"}\" !  gvatrack ! \
+$AGGREGATE gvametaconvert name=metaconvert add-empty-results=true ! \
+gvametapublish method=mqtt file-format=2 address=127.0.0.1:1883 mqtt-client-id=yolov8 topic=event/detection ! \
+queue ! \
+gvametapublish name=destination file-format=2 file-path=/tmp/results/r$cid\"_gst\".jsonl $OUTPUT 2>&1 | tee >/tmp/results/gst-launch_$cid\"_gst\".log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /tmp/results/pipeline$cid\"_gst\".log)"
+
 
 echo "$gstLaunchCmd"
 
