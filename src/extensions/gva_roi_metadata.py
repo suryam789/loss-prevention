17df0b515420b2fa30706d911727b921f921986f
Antonio Martinez <jose.antonio.martinez.torres@intel.com>
2024-10-22 16:03:35 -0700

    Refactoring
    Signed-off-by: Antonio Martinez <jose.antonio.martinez.torres@intel.com>

--- src/extensions/gva_roi_metadata.py
+++ src/extensions/gva_roi_metadata.py
@@ -11,12 +11,12 @@ import json
 from copy import deepcopy
 gi.require_version('Gst', '1.0')
 
+
 class RoiMetadata:
     def __init__(self, disable=False, roi=""):
         self.roi = roi
 
-
-    def process_frame(self,frame):
+    def process_frame(self, frame):
 
         if self.roi == "":
             return True
@@ -25,32 +25,28 @@ class RoiMetadata:
             message_obj = json.loads(message)
 
             if "objects" in message_obj:
-                for message in frame.messages():
-                    message_obj = json.loads(message)
+                frame.remove_message(message)
+                message_objects_array = deepcopy(message_obj["objects"])
 
-                    if "objects" in message_obj:
-                        frame.remove_message(message)
-                        message_objects_array = deepcopy(message_obj["objects"])
+                roi_objects = []
+                other_objects = []
 
-                        roi_objects = []
-                        other_objects = []
+                for obj in message_objects_array:
+                    if "roi_type" in obj:
+                        if obj["roi_type"] == self.roi:
+                            roi_objects.append(obj)
+                        else:
+                            other_objects.append(obj)
 
-                        for obj in message_objects_array:
-                            if "roi_type" in obj:
-                                if obj["roi_type"] == self.roi:
-                                    roi_objects.append(obj)
-                                else:
-                                    other_objects.append(obj)
+                for obj in other_objects:
+                    for objects in roi_objects:
+                        if (obj["x"] >= objects["x"] and obj["x"] + obj["w"] <= objects["x"] + objects["w"] and
+                                obj["y"] >= objects["y"] and obj["y"] + obj["h"] <= objects["y"] + objects["h"]):
+                            obj["roi_type"] = self.roi
 
-                        for obj in other_objects:
-                            for basket in roi_objects:
-                                if (obj["x"] >= basket["x"] and obj["x"] + obj["w"] <= basket["x"] + basket["w"] and
-                                        obj["y"] >= basket["y"] and obj["y"] + obj["h"] <= basket["y"] + basket["h"]):
-                                    obj["roi_type"] = self.roi
+                message_obj["objects"] = other_objects + roi_objects
 
-                        message_obj["objects"] = other_objects + roi_objects
-
-                        frame.add_message(json.dumps(
-                            message_obj, separators=(',', ':')))
-                        break
+                frame.add_message(json.dumps(
+                    message_obj, separators=(',', ':')))
+                break
         return True
