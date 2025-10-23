d3b4b5e4c1d790c1e39778168a3199410816bad4
Arpana Jindal <arpana.jindal@intel.com>
2025-10-23 18:42:14 +0530

    pipeline configuration metrics update

--- src/gst-pipeline-generator.py
+++ src/gst-pipeline-generator.py
@@ -14,7 +14,7 @@ CONFIG_WORKLOAD_TO_PIPELINE = f"/home/pipeline-server/configs/{WORKLOAD_DIST}"
 
 MODELSERVER_DIR = "/home/pipeline-server"
 MODELSERVER_MODELS_DIR = "/home/pipeline-server/models"
-MODELSERVER_VIDEOS_DIR = "/home/pipeline-server/sample-media"
+MODELSERVER_VIDEOS_DIR = "/home/pipeline-server/performance-tools/sample-media"
 
 
 def download_video_if_missing(video_name, width=None, fps=None):
@@ -119,11 +119,11 @@ def build_gst_element(cfg):
     if cfg["type"] == "gvadetect":
         # Always use the precision from the current step config
         model_path = download_model_if_missing(model, "gvadetect", cfg.get("precision", ""))
-        elem = f"gvadetect {name_str} batch-size={BATCH_SIZE_DETECT} {inference_region} model={model_path} device={device} {PRE_PROCESS} {DETECTION_OPTIONS} {PRE_PROCESS_CONFIG}"
+        elem = f"gvadetect {name_str} batch-size={BATCH_SIZE_DETECT} inference-interval=3 scale-method=fast {inference_region} model={model_path} device={device} {PRE_PROCESS} {DETECTION_OPTIONS} {PRE_PROCESS_CONFIG}"
     elif cfg["type"] == "gvaclassify":
         # Always use the precision from the current step config
-        model_path, label_path, proc_path = download_model_if_missing(model, "gvaclassify", cfg.get("precision", ""))     
-        elem = f"gvaclassify {name_str} batch-size={BATCH_SIZE_CLASSIFY} model={model_path} device={device} labels={label_path} model-proc={proc_path} {CLASSIFICATION_PRE_PROCESS}"
+        model_path, label_path, proc_path = download_model_if_missing(model, "gvaclassify", cfg.get("precision", "")) 
+        elem = f"gvaclassify {name_str} batch-size={BATCH_SIZE_CLASSIFY} inference-region=1 scale-method=fast model={model_path} device={device} labels={label_path} model-proc={proc_path} {CLASSIFICATION_PRE_PROCESS}"
     elif cfg["type"] == "gvainference":
         model_path = download_model_if_missing(model, "gvainference", cfg.get("precision", ""))
         elem = f"gvainference  model={model_path} device={device} "
